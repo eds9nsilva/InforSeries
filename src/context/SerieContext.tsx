bec60a02220087ec2ec9fe7dc90a942da491f7a1
eds9nsilva <antonioedson99@gmail.com>
2022-07-26 15:55:08 -0300

    refactor: SerieContext

--- src/context/SerieContext.tsx
+++ src/context/SerieContext.tsx
@@ -17,18 +17,17 @@ export const SerieContextProvider: React.FunctionComponent<IProps> = ({
   children,
 }) => {
   const [series, setSeries] = useState<ISerie[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     async function loadingSeries(number: number) {
       try {
-        setLoading(true);
         const seriesData = await api.get<ISerie[]>(`shows?page=${number}`);
         const data = seriesData.data;
         setSeries(data);
-        setLoading(false);
       } catch (error) {
         throw new Error(error as string);
       }
+      setLoading(false);
     }
     const numberPage = 1;
     loadingSeries(numberPage);
