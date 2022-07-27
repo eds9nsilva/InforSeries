84b809da0c58c43bacb32f52fc6c8db05021a63b
eds9nsilva <antonioedson99@gmail.com>
2022-07-27 17:09:36 -0300

    refactor: * as React for React

--- src/pages/loading/Loading.tsx
+++ src/pages/loading/Loading.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Spinner from 'react-native-spinkit';
 import {colors} from '../../global/styles/colors';
 import Image from '../../assets/logo.png';
