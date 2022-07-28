3dc16073c18b15c5289433dd34e66d6f89c23728
eds9nsilva <antonioedson99@gmail.com>
2022-07-28 17:20:14 -0300

    refactor: add router SeeAll and random Serie Heade

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,21 +2,32 @@ import * as React from 'react';
 import {FlatList, ScrollView} from 'react-native';
 import {SerieContext} from '../../context/SerieContext';
 import Header from './components/Header/Header';
+import {useNavigation} from '@react-navigation/native';
 import {SeriesListHorizontal} from '../../components/SeriesListHorizontal/SeriesListHorizontal';
 import {Container, Title, ContentText, SubTitle} from './styles';
+import {ISerie} from '../../model/Serie';
+
+interface ScreenNavigationProp {
+  navigate: (screem: string, serie: ISerie[]) => void;
+}
 
 export const Home: React.FunctionComponent = () => {
   const {series} = React.useContext(SerieContext);
   const page1 = series.slice(0, 175);
   const page2 = series.slice(175, 250);
+  const navigation = useNavigation<ScreenNavigationProp>();
 
+  function handleNavigate(data: ISerie[]) {
+    navigation.navigate('SeeAll', data);
+  }
+  const number = Math.floor(Math.random() * series.length);
   return (
     <Container>
       <ScrollView>
-        <Header data={series[242]} />
+        <Header data={series[number]} />
         <ContentText>
           <Title>Page view 1</Title>
-          <SubTitle>See all</SubTitle>
+          <SubTitle onPress={() => handleNavigate(page1)}>See all</SubTitle>
         </ContentText>
         <FlatList
           data={page1}
@@ -28,7 +39,7 @@ export const Home: React.FunctionComponent = () => {
         />
         <ContentText>
           <Title>Page view 2</Title>
-          <SubTitle>See all</SubTitle>
+          <SubTitle onPress={() => handleNavigate(page2)}>See all</SubTitle>
         </ContentText>
         <FlatList
           data={page2}
