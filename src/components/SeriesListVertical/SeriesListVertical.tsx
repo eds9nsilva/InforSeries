63bc385d419ea28da4178a71dbe2b36ede7a0c31
eds9nsilva <antonioedson99@gmail.com>
2022-07-28 17:18:12 -0300

    feat: component List for SeeAll

---
+++ src/components/SeriesListVertical/SeriesListVertical.tsx
@@ -0,0 +1,51 @@
+import {useNavigation} from '@react-navigation/native';
+import * as React from 'react';
+import {ISerie} from '../../model/Serie';
+import Spinner from 'react-native-spinkit';
+
+import {
+  Button,
+  ImageBackground,
+  Average,
+  AverageText,
+  ViewLoading,
+  Content,
+} from './styles';
+import {colors} from '../../global/styles/colors';
+
+interface IParams {
+  data: ISerie;
+}
+
+interface ScreenNavigationProp {
+  navigate: (screem: string, serie: ISerie) => void;
+}
+
+export const SeriesListVertical: React.FC<IParams> = ({data}) => {
+  const [loadingImage, setLoadingImage] = React.useState<boolean>(true);
+
+  const navigation = useNavigation<ScreenNavigationProp>();
+
+  function handleNavigate() {
+    navigation.navigate('Details', data);
+  }
+
+  return (
+    <Button onPress={handleNavigate}>
+      {loadingImage && (
+        <ViewLoading>
+          <Spinner type="FadingCircleAlt" size={55} color={colors.white} />
+        </ViewLoading>
+      )}
+      <ImageBackground
+        source={{uri: data.image.medium}}
+        onLoad={() => setLoadingImage(false)}>
+        {data.rating.average !== null && (
+          <Average>
+            <AverageText>{data.rating.average}</AverageText>
+          </Average>
+        )}
+      </ImageBackground>
+    </Button>
+  );
+};
---
+++ src/components/SeriesListVertical/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled.View`
+  align-items: center;
+  flex: 1;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: ${({theme}) => theme.colors.red};
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  height: 280px;
+  width: 48%;
+`;
+
+export const ImageBackground = styled.ImageBackground`
+  height: 100%;
+  width: 100%;
+`;
+
+export const Average = styled.View`
+  margin: 5%;
+  width: 35px;
+  height: 20px;
+  border-radius: 8px;
+  background-color: ${({theme}) => theme.colors.red};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AverageText = styled.Text`
+  font-size: 10px;
+  color: ${({theme}) => theme.colors.white};
+`;
+
+export const ViewLoading = styled.View`
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  z-index: 99;
+`;
