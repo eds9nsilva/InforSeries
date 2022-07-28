949255e2b898e3c73461be3390d95c4850809af3
eds9nsilva <antonioedson99@gmail.com>
2022-07-28 17:20:49 -0300

    fix: bug is isFavorite

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,29 +1,129 @@
-import React from 'react';
-import {ScrollView} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {ScrollView, useWindowDimensions, Share} from 'react-native';
 import {useRoute} from '@react-navigation/native';
+import RenderHtml from 'react-native-render-html';
+import ReadMore from '@fawazahmed/react-native-read-more';
 import {ISerie} from '../../model/Serie';
+import {SerieContext} from '../../context/SerieContext';
+import {Rating} from 'react-native-ratings';
 import {
   Container,
+  Content,
+  ContentDetails,
+  ContentIcon,
+  ContentRating,
   ContentTitle,
   Icon,
   ImageHeader,
-  Summary,
+  Text,
   Title,
+  ViewLoading,
 } from './styles';
 import {colors} from '../../global/styles/colors';
+import Spinner from 'react-native-spinkit';
 
 export const Details: React.FunctionComponent = () => {
+  const {favorites, addFavorite, removeFavorite} = useContext(SerieContext);
+  const [loadingImage, setLoadingImage] = useState<boolean>(true);
+  const [isFavorite, setIsFavorite] = useState<boolean>(true);
   const route = useRoute();
   const data = route.params as ISerie;
+  const verifcarFavorite = favorites.find(series => series.id === data.id);
+  if (verifcarFavorite === undefined) {
+    console.log('É favorito');
+    setIsFavorite(false);
+  }
+  const {width} = useWindowDimensions();
+  const source = {
+    html: `${data.summary}`,
+  };
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'React Native | A framework for building native apps using React',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  /*
+  function handleFavorite() {
+    setIsFavorite(!isFavorite);
+    if (isFavorite) {
+      removeFavorite(data);
+    } else {
+      addFavorite(data);
+    }
+  }*/
   return (
     <Container>
       <ScrollView>
-        <ImageHeader source={{uri: data.image.medium}} />
-        <ContentTitle>
-          <Title>{data.name}</Title>
-          <Icon name="share-2" size={28} />
-        </ContentTitle>
-        <Summary>{data.summary}</Summary>
+        {loadingImage && (
+          <ViewLoading>
+            <Spinner type="FadingCircleAlt" size={66} color={colors.red} />
+          </ViewLoading>
+        )}
+        <ImageHeader
+          source={{uri: data.image.medium}}
+          resizeMode="contain"
+          onLoadEnd={() => setLoadingImage(false)}
+        />
+        <Content>
+          <ContentIcon>
+            <Icon
+              name="heart"
+              size={28}
+              color={isFavorite ? colors.red : colors.white}
+            />
+            <Icon
+              name="share-2"
+              size={28}
+              style={{marginLeft: 16}}
+              onPress={onShare}
+              color={colors.white}
+            />
+          </ContentIcon>
+          <ContentTitle>
+            <Title>{data.name}</Title>
+          </ContentTitle>
+          {data.rating.average !== null && (
+            <ContentRating>
+              <Rating
+                type="custom"
+                readonly
+                imageSize={16}
+                startingValue={data.rating.average}
+                tintColor={colors.gray}
+                ratingCount={10}
+              />
+              <Text style={{marginLeft: 14}}>{data.rating.average}/10</Text>
+            </ContentRating>
+          )}
+          <ContentDetails>
+            <Text>Language: {data.language}</Text>
+            {data.genres && (
+              <Text style={{marginLeft: 14}}>
+                Genres: {data.genres.join(', ')}
+              </Text>
+            )}
+          </ContentDetails>
+        </Content>
+        <RenderHtml
+          contentWidth={width}
+          source={source}
+          baseStyle={{color: colors.white, marginLeft: 10, marginRight: 10}}
+        />
       </ScrollView>
     </Container>
   );
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -1,27 +1,26 @@
 import styled from 'styled-components/native';
-import LinearGradient from 'react-native-linear-gradient';
 import {Feather} from '@expo/vector-icons';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({theme}) => theme.colors.gray};
 `;
+export const Content = styled.View`
+  margin-left: 5%;
+  margin-right: 5%;
+`;
 
 export const ImageHeader = styled.Image`
   width: 100%;
-  height: 400px;
-`;
-
-export const Linear = styled(LinearGradient)`
-  flex-direction: column;
-  justify-content: flex-end;
-  flex: 1;
+  height: 550px;
+  filter: blur(7px);
 `;
 
 export const ContentTitle = styled.View`
   flex-direction: row;
-  margin: 5%;
+  align-items: center;
   justify-content: space-between;
+  max-width: 90%;
 `;
 
 export const Title = styled.Text`
@@ -30,14 +29,33 @@ export const Title = styled.Text`
   font-weight: bold;
 `;
 
-export const Icon = styled(Feather)`
+export const ContentIcon = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-top: 6%;
+`;
+
+export const Icon = styled(Feather)``;
+
+export const ContentRating = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ContentDetails = styled.View`
+  flex-direction: row;
+  align-items: center;
+  max-width: 60%;
+`;
+
+export const Text = styled.Text`
+  font-size: 18px;
   color: ${({theme}) => theme.colors.white};
 `;
 
-export const Summary = styled.Text`
-  font-size: 14px;
-  color: ${({theme}) => theme.colors.white};
-  text-align: left;
-  margin-left: 5%;
-  margin-right: 5%;
+export const ViewLoading = styled.View`
+  top: 25%;
+  left: 40%;
+  position: absolute;
+  z-index: 99;
 `;
